243f423ebcd75a73cda424b83e021c529cb670cb
Aleksandar Kex Trpeski <kex@pristap.com>
2017-06-12 02:23:51 +0200

    Ready for Heroku deployment.

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -7,7 +7,8 @@ defmodule NlbPipeline.UserSocket do
 	channel "control:lobby", NlbPipeline.ControlChannel
 
 	## Transports
-	transport :websocket, Phoenix.Transports.WebSocket
+	transport :websocket, Phoenix.Transports.WebSocket,
+		timeout: 45_000
 	# transport :longpoll, Phoenix.Transports.LongPoll
 
 	# Socket params are passed from the client and can
